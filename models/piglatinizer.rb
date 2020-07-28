33c38943b61c2ae74fed84d1539c46849b7ed474
Jonathan East <jonathan_east@ymail.com>
2020-07-28 19:14:49 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,9 +14,9 @@ class PigLatinizer
     if vowels(word[0])
       "#{word}way"
     else
-      vowel_index = word.index(/[aAeEiIoOuU]/)
-      consonants = word.slice(0..vowel_index-1)
-      word_leftover = word.slice(vowel_index..word.length)
+      v_index = word.index(/[aAeEiIoOuU]/)
+      consonants = word.slice(0..v_index-1)
+      word_leftover = word.slice(v_index..word.length)
       word_leftover + consonants + "ay"
     end 
   end 
