cd4e9e4b8e1fc6a0ef4411a9f11573810784dd50
Jonathan East <jonathan_east@ymail.com>
2020-07-28 19:12:52 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,6 +13,11 @@ class PigLatinizer
   def translate_word(word)
     if vowels(word[0])
       "#{word}way"
+    else
+      vowel_index = word.index(/[aAeEiIoOuU]/)
+      consonants = word.slice(0..vowel_index-1)
+      word_leftover = word.slice(vowel_index..word.length)
+      word_leftover + consonants + "ay"
     end 
   end 
   
